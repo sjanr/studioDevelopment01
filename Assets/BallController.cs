46406ded8936a8eb53d93e8409481762dc159c67
sjanr <97484144+sjanr@users.noreply.github.com>
2025-01-25 13:22:30 -0800

    Added WASD (foward, left, down, right) functionality

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,18 +4,36 @@ using UnityEngine.XR;
 public class BallController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 2f;
     void Start()
     {
-        Debug.Log("Calling the Start method");
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 inputVector = Vector2.zero;
         if (Input.GetKey(KeyCode.W)) {
-        Debug.Log("Calling the Update method");
+            inputVector += Vector2.up;
         }
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            inputVector += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputVector += Vector2.right;
+        }
+
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        sphereRigidbody.AddForce(inputXZPlane);
+
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector: " + inputXZPlane * ballSpeed);
         
     }
 }
