161a7b52d8440783b7a80e89956af24d15ae8bae
sjanr <97484144+sjanr@users.noreply.github.com>
2025-01-25 14:10:44 -0800

    Added jump functionality

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -7,6 +7,8 @@ public class BallController : MonoBehaviour
 
     public Rigidbody sphereRigidbody;
     public float ballSpeed = 2f;
+    public float jumpForce = 10f;
+    public bool isGrounded = true;
     void Start()
     {
         
@@ -34,6 +36,20 @@ public class BallController : MonoBehaviour
 
         Debug.Log("Resultant Vector: " + inputVector);
         Debug.Log("Resultant 3D Vector: " + inputXZPlane * ballSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
         
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        // Reset isGrounded when the sphere touches the ground
+        if (collision.contacts.Length > 0 && collision.contacts[0].point.y <= transform.position.y)
+        {
+            isGrounded = true;
+        }
     }
 }
